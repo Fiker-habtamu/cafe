de0dfc064bc69d165adc90f78892a2d11b09c993
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-06-05 17:51:00 +0300

    just started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import FoodCard from './components/foodCard/FoodCard'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <FoodCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/foodCard/FoodCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './FoodCard.css'
+
+export default function FoodCard() {
+  return (
+	<div className='Container'>
+		<h1>here is the food menu</h1>
+	</div>
+  )
+}
