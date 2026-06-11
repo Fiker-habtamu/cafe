699c6cb4311c2534460752d258f60a0fc77cfb96
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-06-11 17:31:30 +0300

    finish 100% of hero setion and header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,13 +15,13 @@ export default function Header() {
           Your browser does not support the video tag.
         </video>
         {/* hero text */}
-        <div className="hero_text text-center ">
-          <h1 className="text-7xl font-bold uppercase"> Fuel your Daily <br /><span>Hustle.</span> </h1>
-          <p className="mt-10 text-3xl font-light">Experience premium coffee, carefully crafted dishes, and <br /> a space designed for relaxation, work, and <br /> connection.</p>
+        <div className="hero_text text-center">
+          <h1 className="text-7xl font-bold uppercase max-sm:text-5xl!"> Fuel your Daily <br /><span>Hustle.</span> </h1>
+          <p className="mt-10 text-3xl font-light max-sm:text-2xl!">Experience premium coffee, carefully crafted dishes, and <br /> a space designed for relaxation, work, and <br /> connection.</p>
           {/* hero buttons  */}
-          <div className="hero_btns mt-20 text-3xl font-normal">
-            <button color="">Order Now</button>
-            <button>Contact Us</button>
+          <div className="hero_btns mt-20 text-3xl font-normal max-sm:p-2! max-sm:w-80! max-sm:text-2xl! max-sm:mt-10!">
+            <button color="" className="max-sm:w-36!">Order Now</button>
+            <button className="max-sm:w-36!">Contact Us</button>
           </div>
         </div>
       </div>
