4def1862d81c355e53165bafb23a3c905f649d1a
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-06-13 13:06:44 +0300

    finish almost 95% of the development

--- src/components/Pages/SingleDrink/SingleDrink.jsx
+++ src/components/Pages/SingleDrink/SingleDrink.jsx
@@ -1,22 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./SingleDrink.css";
 
 function SingleDrink() {
+  const [singleDrink, setSingleDrink] = useState(null);
   let { id } = useParams();
   useEffect(() => {
-
+    fetch("/drinks.json")
+    .then((response)=>response.json()
+    .then((data)=>{
+      setSingleDrink(data.drinks.find((each)=> each.drink_url == id))
+    })
+  )
   }, []);
-  return (
-    <>
-      <div className="food_card_container flex justify-center items-center">
-		<div>
-        <h1 className="text-4xl text-center">{id} page</h1>
-		<p>this page is under development</p>
-		</div>
 
-      </div>
-    </>
-  );
+
+  if (!singleDrink) {
+    return <div className="text-white text-center mt-10">Loading drink...</div>;
+  } else {
+    const {drink_id,drink_name,drink_url,drink_brief_description,drink_price,drink_image_url,drink_description} = singleDrink
+    return (
+      <>
+        <div className="food_card_container flex justify-center items-center h-fit! ">
+          <div className="singleDrinks_content w-96 p-10 lg:w-10/12! lg:flex lg:justify-between">
+            <div className="single_img lg:w-2/5!">
+              <img
+                src={drink_image_url}
+                alt=""
+                className="lg:h-500px! lg:w-600px!"
+              />
+            </div>
+            <div className="text_contents mt-5 h-fit lg:w-2/5!">
+              <h2 className="text-2xl font-bold uppercase h-15 mb-2">{drink_name}</h2>
+              <p className="text-[#4d4747] h-15 text-[20px ]  mb-2">{drink_brief_description}</p>
+              <p className="text-[20px] mb-5 font-semibold ">ETB: {drink_price}Br</p>
+              <div>
+                <div className="flex justify-center items-center">
+                  <h3 className="text-3xl font-semibold text-black text-shadow-none border-b-2 pb-3">
+                    Detail
+                  </h3>
+                </div>
+                <p className="text-2xl font-medium text-center">
+                 {drink_description}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default SingleDrink;
